27fe821c5b2cf741890e5c7243711ca6add2ec48
Carl Lapierre <carl.lapierre@osedea.com>
2024-03-04 21:16:02 -0500

    Init

---
+++ frontend/src/App.js
@@ -0,0 +1,228 @@
+import React, { useRef, useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import Slider from '@material-ui/core/Slider';
+import confetti from 'canvas-confetti';
+import './App.css'; 
+import CrossFade from 'react-crossfade-image';
+
+
+const colors = ['#FFB3BA', '#FFDFBA', '#FFFFBA', '#BAFFC9', '#BAE1FF', '#BABAFF', '#FFBAF4', '#FFBAB3', '#D3D3D3'];
+const panel = { border: '3px solid #ddd', borderRadius: '10px', backgroundColor: '#fff'};
+const main = 'rgb(63 53 75)';
+const size = 512;
+const refreshRate = 2000;
+const url = process.env.REACT_APP_INFERCENCE_ENDPOINT;
+
+const ColorPicker = ({ setColor, selectedColor }) => {
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
+      {colors.map((color) => (
+        <div
+          	key={color}
+          	onClick={() => setColor(color)}
+          	style={{
+				backgroundColor: color,
+				width: '40px',
+				height: '40px',
+				borderRadius: '50%',
+				cursor: 'pointer',
+				boxShadow: color === selectedColor ? 'inset 0 0 0 4px rgb(145 134 134 / 33%)' : 'none'
+          	}}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+
+function App() {
+  
+	const canvasRef = useRef(null);
+	const contextRef = useRef(null);
+	const [color, setColor] = useState(colors[0]);
+	const [brushSize, setBrushSize] = useState(5);
+	const [textPrompt, setTextPrompt] = useState('');
+	const [isDrawing, setIsDrawing] = useState(false);
+	const [imageSrc, setImageSrc] = useState(null);
+	const [previousCanvasState, setPreviousCanvasState] = useState(null);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		canvas.width = size * 2;  
+		canvas.height = size * 2;
+		canvas.style.width = `512px`;
+		canvas.style.height = `512px`;
+	
+		const context = canvas.getContext("2d");
+		context.scale(2, 2);
+		context.lineCap = "round";
+		contextRef.current = context;
+		setDefault();
+	}, []);
+
+	useEffect(() => {
+		if (contextRef.current) {
+		contextRef.current.strokeStyle = color;
+		contextRef.current.lineWidth = brushSize;
+		}
+	}, [color, brushSize]);
+
+	useEffect(() => {
+        const interval = setInterval(() => {
+            checkCanvasChange();
+        }, refreshRate);
+
+        return () => clearInterval(interval);
+    }, [previousCanvasState]);
+
+	const checkCanvasChange = () => {
+        const currentCanvasState = canvasRef.current.toDataURL();
+
+        if (previousCanvasState && currentCanvasState !== previousCanvasState) {
+            runInference();
+        }
+
+        setPreviousCanvasState(currentCanvasState);
+    };
+
+	const runInference = () => {
+		const prompt = "3d render of a " + textPrompt;
+	
+		const imageDataUrl = canvasRef.current.toDataURL();
+	
+		let data = JSON.stringify({
+			image: imageDataUrl, 
+			prompt: prompt,
+			num_iterations: 5
+		});
+	
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json', 
+			},
+			body: data
+		})
+		.then(response => response.blob())
+		.then(blob => {
+			let url = URL.createObjectURL(blob);
+			updateImage(url);
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+	}
+
+	const startDrawing = ({ nativeEvent }) => {
+		const { offsetX, offsetY } = nativeEvent;
+		contextRef.current.beginPath();
+		contextRef.current.moveTo(offsetX, offsetY);
+		setIsDrawing(true);
+	};
+
+	const finishDrawing = () => {
+		contextRef.current.closePath();
+		setIsDrawing(false);
+	};
+
+	const launchConfetti = () => {
+		confetti({ particleCount: 100, spread: 70, origin: { y: 0.6 } });
+	};  
+
+	const draw = ({ nativeEvent }) => {
+		if (!isDrawing) { return; }
+		const { offsetX, offsetY } = nativeEvent;
+		contextRef.current.lineTo(offsetX, offsetY);
+		contextRef.current.stroke();
+	};
+
+	const setDefault = () => {
+		contextRef.current.fillStyle = "white";
+		contextRef.current.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+	}
+
+	const clearCanvas = () => {
+		const canvas = canvasRef.current;
+		const context = canvas.getContext('2d');
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		setDefault();
+		updateImage(null);
+		launchConfetti();
+	};
+
+	function loadImageOnCanvas() {
+		const image = new Image();
+		image.src = imageSrc;
+		image.onload = () => {
+			const canvas = canvasRef.current;
+			const context = canvas.getContext('2d');
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.drawImage(image, 0, 0); 
+		};
+	}
+
+	const updateImage = (newImageUrl) => {
+		setImageSrc(newImageUrl);
+    }
+  
+	return (
+		<div style={{
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			flexDirection: 'column',
+			height: '100vh',
+			gap: '20px',
+		}}>
+			<div style={ { width: '40%' }}>
+				<input className='prompt border' type="text" value={textPrompt} onChange={(e) => setTextPrompt(e.target.value)} />
+			</div>
+			<div style={{ 
+				display: 'flex', 
+				gap: '20px' 
+			}}>
+				<div style={{ ...panel, height: size }}>
+					<div style={{ display: 'flex', flexDirection: 'column', gap: '10px', padding: '20px'}}>
+						<ColorPicker setColor={setColor} selectedColor={color}/>
+						<div style={{ marginTop: '20px', textAlign: 'center' }}>
+						<div style={{color: main}}>Brush Size</div>
+						<Slider
+							style={{color: main}}
+							value={brushSize}
+							onChange={(e, newValue) => setBrushSize(newValue)}
+							aria-labelledby="brush-size-slider"
+							valueLabelDisplay="auto"
+							min={1}
+							max={100}
+						/>
+						</div>
+						
+						<Button style={{backgroundColor: main}} variant="contained" color="primary" onClick={clearCanvas}>
+							Clear
+						</Button>
+
+						<Button style={{backgroundColor: main}} variant="contained" color="primary" onClick={loadImageOnCanvas}>
+							Transfer
+						</Button>
+					</div>
+				</div>
+				
+				<canvas
+					onMouseDown={startDrawing}
+					onMouseUp={finishDrawing}
+					onMouseMove={draw}
+					ref={canvasRef}
+					style={{ ...panel}}
+				/>
+
+				<div className='image-container' style={{ ...panel, width: size, height: size }}>
+					<CrossFade src={imageSrc} duration={400} timingFunction="ease-in-out" delay={0} alt="Original Image" />
+				</div>
+			</div>
+			
+		</div>
+	);
+
+}
+
+export default App;
---
+++ turbo.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+from fastapi import File, Form, Response, Request
+from modal import Image, Mount, Stub, gpu, web_endpoint, build, enter, method
+
+inference_image = Image.debian_slim().pip_install(
+    "Pillow~=10.1.0",
+    "diffusers~=0.24.0",
+    "transformers~=4.35.2", 
+    "accelerate~=0.25", 
+    "safetensors~=0.4.1",  
+)
+
+stub = Stub("stable-diffusion-xl-turbo", image=inference_image)
+
+with inference_image.imports():
+    import torch
+    from diffusers import AutoPipelineForImage2Image
+    from diffusers.utils import load_image
+    from huggingface_hub import snapshot_download
+    from PIL import Image as PILImage
+    from io import BytesIO
+    import base64
+
+@stub.cls(gpu=gpu.A10G(), container_idle_timeout=240)
+class Model:
+    @build()
+    def download_models(self):
+
+        ignore = [
+            "*.bin",
+            "*.onnx_data",
+            "*/diffusion_pytorch_model.safetensors",
+        ]
+
+        snapshot_download("stabilityai/sdxl-turbo", ignore_patterns=ignore)
+
+    @enter()
+    def enter(self):
+        self.pipe = AutoPipelineForImage2Image.from_pretrained(
+            "stabilityai/sdxl-turbo",
+            torch_dtype=torch.float16,
+            variant="fp16",
+            device_map="auto",
+        )
+
+    @web_endpoint(method="POST")
+    async def inference(self, request: Request):
+        data = await request.json()
+        
+        image = data.get("image")
+        num_iterations = data.get("num_iterations")
+        prompt = data.get("prompt")
+
+        img_data_in = base64.b64decode(image.split(",")[-1])
+
+        byte_stream = BytesIO(img_data_in)
+        pil_image = PILImage.open(byte_stream)
+        init_image = load_image(pil_image).resize((512, 512))
+
+        num_inference_steps = int(num_iterations)
+        strength = 0.999 if num_iterations == 2 else 0.65
+        assert num_inference_steps * strength >= 1
+
+        output = self.pipe(
+            prompt,
+            image=init_image,
+            num_inference_steps=num_inference_steps,
+            strength=strength,
+            guidance_scale=0.0
+        ).images[0]
+
+        print("Image generated", output.size, output.mode, output.format, output.info)
+
+        byte_stream = BytesIO()
+        output.save(byte_stream, format="jpeg")
+        img_data_out = byte_stream.getvalue()
+
+        return Response(content=img_data_out, media_type="image/jpeg")
